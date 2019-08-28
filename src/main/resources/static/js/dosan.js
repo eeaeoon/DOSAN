256ed75984840a31acd45f47b73d336737c97af3
Duhoon <eclip6@naver.com>
2019-08-28 11:17:34 +0900

    yeah

--- src/main/resources/static/js/dosan.js
+++ src/main/resources/static/js/dosan.js
@@ -2,21 +2,24 @@ $(function(){
     $("input:radio").click(function(){
         if ($("input:radio[value='해외송금']").is(":checked")){
             $("select").attr('disabled', false);
-            $("input[id='nt-send']").attr('disabled', false);
+            $("#int_send").attr('disabled', false);
+            $("#nt_send").attr('disabled', true);
+            
         }
         if ($("input:radio[value='해외송금']").is(":checked")== false){
-            $("select").attr('di sabled', true);
-            $("input[name='send_value']").attr('disabled', true);
+            $("select").attr('disabled', true);
+            $("#nt_send").attr('disabled', false);
+            $("#int_send").attr('disabled', true);
         }
     })
 
-    $("#exchange_total").keyup(function(){
+    $("#int_send").keyup(function(){
         var nation = $("#national option:selected").val();
         var money = $("#int_send").val();
         var sum;
         if (nation =="중국"){
             var exchange_rate = 169.34;
-            sum = money / exchange_rate; 
+            sum = money / exchange_rate;
         }
         else if (nation == "미국"){
             var exchange_rate = 1213.40;
@@ -28,5 +31,4 @@ $(function(){
         }
         $("input[name='send_value2']").val(sum);
     })
-    
 });
